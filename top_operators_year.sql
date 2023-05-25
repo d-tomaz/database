99b3d80e8bb3353e615fcdb126fc8d6ee4748887
Daniel Tomaz <49313643+d-tomaz@users.noreply.github.com>
2023-05-25 10:44:08 -0300

    Get records from table based on certain criteria

---
+++ top_operators_year.sql
@@ -0,0 +1,19 @@
+-- Consulta para obter os registros da tabela "demonstracoes_contabeis" que atendem a certos critérios
+
+SELECT reg_ans, vl_saldo_final  -- Seleciona as colunas "reg_ans" e "vl_saldo_final" para serem exibidas no resultado
+FROM demonstracoes_contabeis  -- Especifica a tabela alvo da consulta como "demonstracoes_contabeis"
+WHERE descricao IN ('EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR ', 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÃŠNCIA A SAÃšDE MEDICO HOSPITALAR ')  -- Filtra as linhas onde a coluna "descricao" está presente em determinados valores
+    AND data BETWEEN '2022-01-01' AND '2022-12-31'  -- Filtra as linhas onde a coluna "data" está dentro do intervalo especificado (entre '2022-01-01' e '2022-12-31')
+GROUP BY reg_ans, vl_saldo_final  -- Agrupa os resultados pelo valor das colunas "reg_ans" e "vl_saldo_final"
+HAVING vl_saldo_final = MIN(vl_saldo_final)  -- Restringe os resultados apenas aos grupos onde o valor de "vl_saldo_final" é o mínimo dentro do grupo
+ORDER BY vl_saldo_final  -- Ordena os resultados com base no valor de "vl_saldo_final" em ordem ascendente
+LIMIT 10;  -- Limita o número de resultados retornados para 10
+
+SELECT reg_ans, vl_saldo_final
+FROM demonstracoes_contabeis   
+WHERE descricao IN ('EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR ', 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÃŠNCIA A SAÃšDE MEDICO HOSPITALAR ')
+    AND data BETWEEN '2021-01-01' AND '2021-12-31'
+GROUP BY reg_ans, vl_saldo_final
+HAVING vl_saldo_final = MIN(vl_saldo_final)
+ORDER BY vl_saldo_final
+LIMIT 10;
